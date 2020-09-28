a4e997b76f9895cf5e678a46df62a8e3eef862b9
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-28 14:26:08 -0700

    added another madlib and link to connect to
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- MadLibs/Controllers/HomeControllers.cs
+++ MadLibs/Controllers/HomeControllers.cs
@@ -13,7 +13,33 @@ namespace MadLibs.Controllers
       myMadlibChoice.Cake = "cake";
       return View( myMadlibChoice);
     }
-      
+    [Route("/DisneyForm")] //Defaults to form
+    public ActionResult DisneyForm() { return View(); }
+    
+    [Route("/DisneyMadLib")]
+    public ActionResult DisneyMadlib(string friendsName1, string numberOfHours, string vehicle, string adjective1, string adjective2, string ingVerb, string animal, string adjective3, string pastTenseVerb1, string adjective4, string noun1, string friendsName2, string pastTenseVerb2, string pastTenseVerb3, string place, string verb1)
+    {
+      DisneyWordVariables myDisneyWordVariables = new DisneyWordVariables();
+      myDisneyWordVariables.FriendsName1 = friendsName1;
+      myDisneyWordVariables.NumberOfHours = numberOfHours;
+      myDisneyWordVariables.Vehicle = vehicle;
+      myDisneyWordVariables.Adjective1 = adjective1;
+      myDisneyWordVariables.Adjective2 = adjective2;
+      myDisneyWordVariables.IngVerb = ingVerb;
+      myDisneyWordVariables.Animal = animal;
+      myDisneyWordVariables.Adjective3 = adjective3;
+      myDisneyWordVariables.PastTenseVerb1 = pastTenseVerb1;
+      myDisneyWordVariables.Adjective4 = adjective4;
+      myDisneyWordVariables.Noun1 = noun1;
+      myDisneyWordVariables.FriendsName2 = friendsName2;
+      myDisneyWordVariables.PastTenseVerb2 = pastTenseVerb2;
+      myDisneyWordVariables.PastTenseVerb3 = pastTenseVerb3;
+      myDisneyWordVariables.Place = place;
+      myDisneyWordVariables.Verb1 = verb1;
+
+      return View(myDisneyWordVariables);
+    }
+
     [Route("/form")] //Defaults to form
     public ActionResult Form() { return View(); }
 
--- MadLibs/Models/WordVariables.cs
+++ MadLibs/Models/WordVariables.cs
@@ -10,6 +10,27 @@ namespace MadLibs.Models
     public string PluralNoun3 { get; set; }
     public string Noun2 { get; set; }
     public string PluralNoun4 { get; set; }
+    
+  }
+
+  public class DisneyWordVariables
+  {
+    public string FriendsName1 { get; set; }
+    public string NumberOfHours { get; set; }
+    public string Vehicle { get; set; }
+    public string Adjective1 { get; set; }
+    public string Adjective2 { get; set; }
+    public string Animal { get; set; }
+    public string Adjective3 { get; set; }
+    public string PastTenseVerb1 { get; set; }
+    public string Adjective4 { get; set; }
+    public string PastTenseVerb2 { get; set; }
+    public string PastTenseVerb3 { get; set; }
+    public string Place { get; set; }
+    public string FriendsName2 { get; set; }
+    public string IngVerb { get; set; }
+    public string Verb1 { get; set; }
+    public string Noun1 { get; set; }
   }
 
   public class MadlibChoice
